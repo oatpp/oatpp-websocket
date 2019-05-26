65c715dc558ad78e7e60af557382807256410f63
lganzzzo <lganzzzo@gmail.com>
2019-05-26 23:42:31 +0300

    Updated to latest oatpp APIs

--- test/oatpp-websocket/tests.cpp
+++ test/oatpp-websocket/tests.cpp
@@ -10,21 +10,8 @@
 #include "oatpp-websocket/Handshaker.hpp"
 
 #include <iostream>
-#include <mutex>
 
 namespace {
-
-class Logger : public oatpp::base::Logger {
-private:
-  oatpp::concurrency::SpinLock m_lock;
-public:
-
-  void log(v_int32 priority, const std::string& tag, const std::string& message) override {
-    std::lock_guard<oatpp::concurrency::SpinLock> lock(m_lock);
-    std::cout << tag << ":" << message << "\n";
-  }
-
-};
 
 class Test : public oatpp::test::UnitTest {
 public:
@@ -49,11 +36,8 @@ void runTests() {
 int main() {
 
   oatpp::base::Environment::init();
-  oatpp::base::Environment::setLogger(new Logger());
 
   runTests();
-
-  oatpp::base::Environment::setLogger(nullptr);
 
   /* Print how much objects were created during app running, and what have left-probably leaked */
   /* Disable object counting for release builds using '-D OATPP_DISABLE_ENV_OBJECT_COUNTERS' flag for better performance */
